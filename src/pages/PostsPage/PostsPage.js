499fd896a3821118e0981d9c62644fd1aab51321
ling183 <l.grakauskaite@gmail.com>
2023-03-12 10:57:35 +0200

    posts data

--- src/pages/PostsPage/PostsPage.js
+++ src/pages/PostsPage/PostsPage.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const PostsPage = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/posts')
+      .then(res => res.json())
+      .then(postsData => {
+        setPosts(postsData);
+      })
+  }, []);
+
   return (
-    <h1>Posts</h1>
+    <div>
+      <h1>Posts</h1>
+      {posts && (
+        <ul>
+          {posts.map((post, index) => <li key={index}><Link to={'/posts/' + post.id}>{post.title}</Link></li>)}
+        </ul>
+      )}
+    </div>
   )
 }
 
